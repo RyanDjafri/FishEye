1d3a13bf9765ed9490fb15e99f60c8b1973d6f90
RyanDjafri <ryandjafripro@gmail.com>
2023-10-12 14:58:05 +0200

    --style, home, photographer-page, modal, carousel, likes, sort, accessibility

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,5 +1,7 @@
 import { fetchData } from "../api/index.js";
 
+let cards = [];
+
 const DOM = () => {
   const logo = document.querySelector(".logo");
   logo.addEventListener("click", () => {
@@ -27,24 +29,24 @@ const getData = async () => {
         };
       } else {
         console.error("Photographer not found for ID:", photographerId);
-        return null; // Return null if photographer is not found
+        return null;
       }
     } else {
       console.error("PhotographerId not found in the URL.");
-      return null; // Return null if photographerId is not found
+      return null;
     }
   } catch (error) {
     console.error("Error fetching photographer data:", error);
-    return null; // Return null in case of an error
+    return null;
   }
 };
 
 const displayPhotograph = async () => {
   const data = await getData();
   const main = document.getElementById("main");
+
   if (data) {
     const photograph = data.photographer;
-    console.log(photograph);
     const picture = `assets/photographers/${photograph.name.replace(
       /[\s-]+/g,
       ""
@@ -77,7 +79,6 @@ const displayMedia = async (sortBy) => {
   const data = await getData();
   if (data) {
     const hisMedia = data.hisMedia;
-    console.log(hisMedia);
     const sortingFunction = (a, b) => {
       if (sortBy === "popularite") {
         return b.likes - a.likes;
@@ -98,35 +99,39 @@ const displayMedia = async (sortBy) => {
       if (media.image) {
         const picture = `assets/media/${media.image}`;
         mediaElement = `
-            <div class="card-picture">
-              <img src="${picture}" alt="${picture}" class="media-picture"/>
-            </div>
-          `;
+          <div class="card-picture">
+            <img src="${picture}" alt="${picture}" class="media-picture"/>
+          </div>
+        `;
       } else if (media.video) {
         const videoSource = `./assets/media/${media.video}`;
         mediaElement = `
-            <div class="card-picture">
-              <video class="video">
-                <source src="${videoSource}" type="video/mp4" >
-              </video>
-            </div>
-          `;
-      }
-      rowHTML += `
-          <div class="card">
-            ${mediaElement}
-            <div class="card-info">
-              <p class="card-p">${media.title}</p>
-              <span class="card-s">${media.likes}<i class="fa-solid fa-heart"></i></span>
-            </div>
+          <div class="card-picture">
+            <video class="video">
+              <source src="${videoSource}" type="video/mp4">
+            </video>
           </div>
         `;
+      }
+      rowHTML += `
+        <div class="card">
+          ${mediaElement}
+          <div class="card-info">
+            <p class="card-p">${media.title}</p>
+            <span class="card-s">${media.likes}<i class="fa-solid fa-heart"></i></span>
+          </div>
+        </div>
+      `;
 
       if ((index + 1) % 3 === 0 || index === hisMedia.length - 1) {
         cardsContainer.innerHTML += `<div class="row">${rowHTML}</div>`;
         rowHTML = "";
       }
     });
+
+    cards = document.querySelectorAll(".card");
+
+    getSource();
   }
 };
 
@@ -135,30 +140,26 @@ displayMedia("popularite");
 displayPhotograph();
 
 const carouselModal = document.querySelector(".carousel");
-const img = carouselModal.querySelector("img");
+const img = carouselModal.querySelector(".image-opened");
 const video = carouselModal.querySelector("video");
 const prev = document.querySelector(".previous");
 const next = document.querySelector(".next");
 const close = document.querySelector(".close");
 let imageIndex = 0;
 
-const cardsContainer = document.querySelector(".cards-container");
-const cards = cardsContainer.querySelectorAll(".card");
-
 function getSource() {
+  const cardsContainer = document.querySelector(".cards-container");
+  const cards = cardsContainer.querySelectorAll(".card");
   cards.forEach((card, index) => {
-    const cardPicture = card.querySelector(".card-picture img");
-    const cardVideo = card.querySelector(".card-picture video source");
     card.addEventListener("click", () => {
       carouselModal.showModal();
       imageIndex = index;
-      updateMedia();
+      updateMedia(cards);
     });
   });
 }
-getSource();
 
-const updateMedia = () => {
+const updateMedia = (cards) => {
   const card = cards[imageIndex];
   const cardPicture = card.querySelector(".card-picture img");
   const cardVideo = card.querySelector(".card-picture video source");
@@ -182,7 +183,7 @@ prev.addEventListener("click", () => {
   } else {
     imageIndex = cards.length - 1;
   }
-  updateMedia();
+  updateMedia(cards);
 });
 
 next.addEventListener("click", () => {
@@ -191,7 +192,7 @@ next.addEventListener("click", () => {
   } else {
     imageIndex = 0;
   }
-  updateMedia();
+  updateMedia(cards);
 });
 
 close.addEventListener("click", () => {
