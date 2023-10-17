b91e064ea3d04e5bc619fae22f1525174c41e670
RyanDjafri <ryandjafripro@gmail.com>
2023-10-17 09:44:40 +0200

    responsive home page

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,6 +1,6 @@
 const btn = document.querySelector(".contact_button");
 const modal = document.querySelector("#contact_modal");
-const close = document.querySelector(".close");
+const close = document.querySelector(".close-btn");
 btn.addEventListener("click", () => {
   modal.showModal();
 });
