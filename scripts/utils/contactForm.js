758b9582496c7c5a50bce1c9956394e34d400cfe
RyanDjafri <ryandjafripro@gmail.com>
2023-10-21 11:25:09 +0200

    --carousel

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,5 +1,5 @@
 const btn = document.querySelector(".contact_button");
-const modal = document.querySelector("#contact_modal");
+const modal = document.querySelector(".contact_modal");
 const close = document.querySelector(".close-btn");
 btn.addEventListener("click", () => {
   modal.showModal();
